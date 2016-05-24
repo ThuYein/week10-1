189231e070e5c19f9f375db6a437df3e5c35459d
junbinku <junbin.ku@my.jcu.edu.au>
2016-05-24 13:28:41 +0800

    Add files via upload

---
+++ exercise2.py
@@ -0,0 +1,32 @@
+import random
+
+__author__ = 'Lindsay Ward'
+MAX_INCREASE = 0.175  # 17.5%
+MAX_DECREASE = 0.05  # 5%
+MIN_PRICE = 1.0
+MAX_PRICE = 100.0
+INITIAL_PRICE = 10.0
+
+def format_currency(price):
+   return "${:,.2f}".format(price)
+
+price = INITIAL_PRICE
+#print ("${:,.2f}".format(price))
+
+
+while price >= MIN_PRICE and price <= MAX_PRICE:
+    priceChange = 0
+    # generate a random integer of 1 or 2
+    # if it's 1, the price increases, otherwise it decreases
+    if random.randint(1, 2) == 1:
+        # generate a random floating-point number
+        # between 0 and MAX_INCREASE
+        priceChange = random.uniform(0, MAX_INCREASE)
+    else:
+        # generate a random floating-point number
+        # between negative MAX_INCREASE and 0
+        priceChange = random.uniform(-MAX_DECREASE, 0)
+
+    price *= (1 + priceChange)
+    #print("${:,.2f}".format(price))
+    print format_currency(price)
